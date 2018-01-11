36f1431f87f0fdd4984313a7e209f5eae449ae16
Evgeniy Kubesh <emanager@top-status.ru>
2018-01-11 10:55:34 +0300

    A4-T50 added editor config file and applied format style for index. Also was included material dialog module

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,32 @@ import { CommonModule } from '@angular/common';
 import { FsDialogService } from './fsdialog.service';
 import { FsDialogComponent } from './fsdialog.component';
 
+// Material
+import { MatDialogModule } from '@angular/material/dialog';
+
 export * from './fsdialog.service';
 export * from './fsdialog.component';
 
 @NgModule({
   imports: [
     CommonModule,
-],
-declarations: [
-    FsDialogComponent
-],
-providers: [
-    FsDialogService
-],
-exports: [
-    FsDialogComponent
-]
+    MatDialogModule,
+  ],
+  declarations: [
+    FsDialogComponent,
+  ],
+  providers: [
+    FsDialogService,
+  ],
+  exports: [
+    FsDialogComponent,
+  ]
 })
 export class FsDialogModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: FsDialogModule,
-      providers: [ FsDialogService ]
+      providers: [FsDialogService]
     };
   }
 }
