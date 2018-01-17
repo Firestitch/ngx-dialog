a367089d2825932bca75c03195bbfad2f4e234db
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-01-17 17:50:08 +0300

    updated build files

--- release/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './fs-dialog.module';
-export * from './configs';
-export * from './fs-confirm';
-export * from './fs-input';
-export * from './fs-prompt-autocomplete';
-export * from './fs-prompt-select';
-export * from './fsprompt.service';
