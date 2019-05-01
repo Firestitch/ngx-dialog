aadf8f2a517f78c9f03e07c5c87c285476eeab8d
Raymond Gigliotti <ray@firestitch.com>
2019-05-01 16:27:44 -0400

    remove side margin from buttons

--- playground/app/components/basic-dialog/basic-dialog.component.ts
+++ playground/app/components/basic-dialog/basic-dialog.component.ts
@@ -9,7 +9,6 @@ export class BasicDialogComponent {
 
   public object: any = {};
   public input = '';
-  public paragraphs = [];
   public mobileMode;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
@@ -25,7 +24,7 @@ export class BasicDialogComponent {
     this.dialogRef.close();
   }
 
-  addParagraph() {
-    this.paragraphs.push(this.paragraphs.length);
+  addText() {
+    this.input = this.input + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget consequat nisl, eget egestas orci. Aliquam hendrerit dui eu mauris luctus vehicula. Sed pulvinar lacinia enim vitae varius. Etiam finibus justo ut sapien condimentum, et molestie neque fermentum. Proin mattis erat augue, vitae tempor sem imperdiet in. Cras a eleifend nisi. Praesent accumsan lectus eu ligula volutpat, sed malesuada sapien vestibulum. Morbi rhoncus hendrerit condimentum. Phasellus nunc turpis, ultrices sodales arcu nec, aliquam mattis ipsum.';
   }
 }
