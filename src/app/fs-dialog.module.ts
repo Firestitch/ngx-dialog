26a45810db2709b3290dce46d1b53a35d6bbf1f0
Raymond Gigliotti <ray@firestitch.com>
2019-05-08 16:08:06 -0400

    moved min width to styles and not matdialog

--- src/app/fs-dialog.module.ts
+++ src/app/fs-dialog.module.ts
@@ -25,7 +25,6 @@ export class FsDialogModule {
             panelClass: 'fs-dialog-backdrop',
             maxWidth: '95vw',
             disableClose: false,
-            minWidth: '400px',
             autoFocus: true,
             hasBackdrop: true
           }
