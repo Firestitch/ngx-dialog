a37f74f3710e370cb9bea09d2f19989fda165657
Raymond Gigliotti <ray@firestitch.com>
2022-04-27 09:48:29 -0400

    calculated steps back based on the current component path. I think the proper way would be to get the path from the component: FsDialogRouteComponent to the active route and count the / + 1. I think for now the logic I have in the code works for 90% of the time.

--- src/app/components/route/route.component.ts
+++ src/app/components/route/route.component.ts
@@ -90,23 +90,25 @@ export class FsDialogRouteComponent implements OnInit, OnDestroy {
   }
 
   private _navigateOutFromDialog(): void {
-    let stepsBack = 1;
-    let route = this._route.parent;
+    // let stepsBack = 1;
+    // let route = this._route.parent;
 
-    // we are looking for parent route to navigate from current dialog
-    // but we have to skip routes like loadChildren or redirectTo
-    // because it does not make sence to do navigation to them
-    while (route.routeConfig && (route.routeConfig.redirectTo || route.routeConfig.path === '') && route.parent) {
-      stepsBack++;
+    // // we are looking for parent route to navigate from current dialog
+    // // but we have to skip routes like loadChildren or redirectTo
+    // // because it does not make sence to do navigation to them
+    // while (route.routeConfig && (route.routeConfig.redirectTo || route.routeConfig.path === '') && route.parent) {
+    //   stepsBack++;
 
-      if (route.routeConfig.path === '' && route.parent?.routeConfig.loadChildren && route.parent?.parent) {
-        route = route.parent.parent
-      } else {
-        route = route.parent;
-      }
-    }
+    //   if (route.routeConfig.path === '' && route.parent?.routeConfig.loadChildren && route.parent?.parent) {
+    //     route = route.parent.parent
+    //   } else {
+    //     route = route.parent;
+    //   }
+    // }
 
-    // make relative navigation path like '../../../';
+    const stepsBack = (this._route.routeConfig.path.match(/\//g) || []).length + 1; 
+
+    // // make relative navigation path like '../../../';
     const navigationPath = Array
       .from(Array(stepsBack), () => '../')
       .join('');
